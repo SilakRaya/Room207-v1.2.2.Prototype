321da9ef27404363715af924e6ffab04f9f383c2
TomanoJuice <iandaniel30009@gmail.com>
2026-05-20 16:12:01 +0800

    Bshy bushy

--- room-207-prototype/EpisodeOne/ASSets/bush.gd
+++ room-207-prototype/EpisodeOne/ASSets/bush.gd
@@ -1,8 +1,17 @@
 extends Node3D
 
 
-func _on_bush_area_1_body_entered(jolina):
-	print("Jolina has entered the bush")
-	
-func _on_bush_area_2_body_entered(jolina):
-	print("Jolina has entered a bush again")
+func _on_body_entered(body):
+	if body.is_in_group("jolina"):
+		print("Player Has entered Bush")
+
+func _on_body_exited(body):
+	if body.is_in_group("jolina"):
+		print("Player exited bush")
+
+func _on_child_entered_tree(node: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_child_exiting_tree(node: Node) -> void:
+	pass # Replace with function body.
